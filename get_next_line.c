a0de86de65efd7223b7de12aa0dce72884db60f6
Alexandre STEPHAN <alesteph@student.42.fr>
2018-11-19 19:45:13 +0100

    norme

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,13 @@
 /*   By: alesteph <alesteph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 17:11:31 by alesteph          #+#    #+#             */
-/*   Updated: 2018/11/19 17:05:37 by alesteph         ###   ########.fr       */
+/*   Updated: 2018/11/19 19:16:08 by alesteph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h> //debug
 
-int		end_of_line(t_list *lst, int fd)
+static int	end_of_line(t_list *lst, int fd)
 {
 	int		i;
 	char	*str;
@@ -28,16 +27,14 @@ int		end_of_line(t_list *lst, int fd)
 	return (-1);
 }
 
-char	*send_line(t_list **lst, int fd)
+static char	*send_line(t_list **lst, int fd)
 {
 	char	*str;
-	int		len;
 	int		end;
 
 	end = end_of_line(*lst, fd);
 	while (*lst && (*lst)->content_size != (size_t)fd)
 		*lst = (*lst)->next;
-	len = ft_strlen((*lst)->content);
 	if (!(str = (char *)malloc(sizeof(char) * end + 1)))
 		return (NULL);
 	str = ft_strncpy(str, (*lst)->content, end);
@@ -46,11 +43,27 @@ char	*send_line(t_list **lst, int fd)
 	return (str);
 }
 
-int		get_next_line(const int fd, char **line)
+static void	read_it(t_list **lst, const char *buffer, int rd, int fd)
+{
+	char	*tmp;
+
+	if (!*lst)
+	{
+		*lst = ft_lstnew(buffer, rd + 1);
+		(*lst)->content_size = (size_t)fd;
+	}
+	else
+	{
+		tmp = (*lst)->content;
+		(*lst)->content = (void *)ft_strjoin((*lst)->content, buffer);
+		free(tmp);
+	}
+}
+
+int			get_next_line(const int fd, char **line)
 {
 	int				rd;
 	static t_list	*lst = NULL;
-	char			*tmp;
 	char			buffer[BUFF_SIZE + 1];
 
 	ft_bzero(buffer, BUFF_SIZE + 1);
@@ -60,17 +73,8 @@ int		get_next_line(const int fd, char **line)
 			return (-1);
 		return (1);
 	}
-	rd = read(fd, buffer, BUFF_SIZE);
-	if (!lst)
-	{
-		lst = ft_lstnew(buffer, rd + 1);
-		lst->content_size = (size_t)fd;
-	}
-	else
-	{
-		tmp = lst->content;
-		lst->content = (void *)ft_strjoin(lst->content, buffer);
-		free(tmp);
-	}
+	if ((rd = read(fd, buffer, BUFF_SIZE)) < 0)
+		return (-1);
+	read_it(&lst, buffer, rd, fd);
 	return ((rd > 0) ? get_next_line(fd, line) : 0);
 }
