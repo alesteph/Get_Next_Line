b7de4a049cba21464c2af6f2b28ce5dda90b3fca
Alexandre STEPHAN <alesteph@student.42.fr>
2018-11-20 11:46:54 +0100

    main perso

---
+++ main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alesteph <alesteph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/14 17:01:31 by alesteph          #+#    #+#             */
+/*   Updated: 2018/11/20 11:41:56 by alesteph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int		main(int ac, char **av)
+{
+	int		fd;
+	char	*line;
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		write(1, "aaa", 3);
+		while (get_next_line(1, &line) > 0)
+		{
+			ft_putendl(line);
+			free(line);
+		}
+		close(fd);
+	}
+	else
+		ft_putstr_fd("Missing file.\n", 2);
+	return (0);
+}
